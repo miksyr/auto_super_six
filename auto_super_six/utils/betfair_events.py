988f5d2e23da198fc45e6ffa2992396dd003e1a6
Mike Doran <mikrdoran@gmail.com>
2023-02-16 15:51:02 +0000

    .

--- auto_super_six/utils/betfair_events.py
+++ auto_super_six/utils/betfair_events.py
@@ -10,7 +10,7 @@ def _match_by_text_query(
         marketTypes=[MarketTypes.FULLTIME_SCORELINE],
         competitionIds=[competition_id],
         textQuery=text_query,
-        daysAhead=2,
+        daysAhead=5,
     )
     if len(betfair_events) != 1:
         raise IndexError(f"{len(betfair_events)} events found for {text_query}")
